cdee207fea84980e630a0fdf174a4562466b4443
Courtney Miles <courtney@milesasylum.com>
2014-10-29 18:09:54 +1000

    Restart Apache after option changes

--- 31-modify-apache-conf.sh
+++ 31-modify-apache-conf.sh
@@ -2,3 +2,5 @@
 
 # Adds modules for Fast CGI and Apache status.
 sed -i "s/\(APACHE2_OPTS=\"\)\(.*\)\(\"\)/\1\2 -D PROXY_FCGI -D FASTCGI_HANDLER -D STATUS\3/" /etc/conf.d/apache2
+
+/etc/init.d/apache2 restart
