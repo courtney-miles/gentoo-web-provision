c15e0326da23983d79bfe539a94413b64738763a
Courtney Miles <courtney@milesasylum.com>
2015-03-29 16:54:14 +1000

    Shift setting of locale until after the first emerge.

--- 00-gentoo-prep.sh
+++ 00-gentoo-prep.sh
@@ -10,13 +10,6 @@ echo "127.0.0.1" `hostname` >> /etc/hosts
 #cp /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
 #echo "Australia/Brisbane" >> /etc/timezone
 
-# Set locale
-echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_AU.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-sed -i "s/en_US.utf8/en_GB.utf8/g" /etc/env.d/02locale
-env-update && source /etc/profile
-
 # Grab a fixed portage snapshot instead of emerge-webrsync so we can ensure these script will work tomorrow.
 echo "Downloading portage snaphshot..."
 PORTAGE_DATE=20150320
@@ -30,3 +23,10 @@ emerge $EM_VERBOSE --update portage
 emerge $EM_VERBOSE --update --newuse --deep --with-bdeps=y --keep-going @world
 
 revdep-rebuild
+
+# Set locale
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_AU.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+sed -i "s/en_US.utf8/en_GB.utf8/g" /etc/env.d/02locale
+env-update && source /etc/profile
